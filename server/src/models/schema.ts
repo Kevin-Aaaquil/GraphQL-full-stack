d1f258521b289f867e294d918de64f8400b61f41
Kevin-Aaaquil <eshansingh2409@gmail.com>
2021-08-05 10:26:20 +0530

    data now comes from db

--- server/src/models/schema.ts
+++ server/src/models/schema.ts
@@ -1,38 +1,18 @@
 import   {graphql, GraphQLID, GraphQLInt, GraphQLList, GraphQLObjectType,GraphQLSchema, GraphQLString } from "graphql";
-import _ from 'lodash'
-
-
-// dummy data and I am a big fan of PERCY JAKSON
-let books = [
-    {name : "Percy Jakson", genre : "fantasy", id : "1", authorID : '1'},
-    {name : "Robert Langdon", genre : "fantasy", id : "2", authorID : '2'},
-    {name : "Kevin Aaaquil", genre : "fantasy", id : "3", authorID : '3'},
-    {name : "ShadowHunters", genre : "fantasy", id : "4", authorID : '4'},
-    {name : "Celaena Sardothien", genre : "fantasy", id : "5", authorID : '5'},
-    {name : "The Heroes Of Olympus", genre :"fantasy", id:6, authorID : '1'},
-    {name : "The Kane Chronicles", genre : "fantasy", id:7, authorID : '1'},
-    {name : "The Trials Of Apollo", genre :"fantasy", id:8, authorID : '1'},
-]
-
-let authors = [
-    {name : "Rick Riordan", age:57, id:"1"},
-    {name : "Dan Brown", age:57, id : "2" },
-    {name : "Eshan Singh", age:18, id : "3"},
-    {name : "Cassandra Claire", age : 48, id:"4"},
-    {name: "Sarah J. Maas", age : 35, id:"5"}
-]
-
+// import _ from 'lodash'
+import DB from '../db'
 
 const BookType = new GraphQLObjectType({
     name : "Book", 
     fields : ()=>({
         id: {type : GraphQLID},
         name : {type : GraphQLString},
-        genre : {type : GraphQLString},
+        main : {type : GraphQLString},
         author : {
             type : AuthorType,
             resolve(parent, args){
-                return _.find(authors,{id : parent.authorID})
+               // return _.find(authors,{id : parent.authorID})
+             return  DB().then(DB => DB.collection("authors").find({id : parent.authorID})).then(async (data)  =>{ return await data.toArray()})
             }
         }
     })
@@ -47,7 +27,8 @@ const AuthorType = new GraphQLObjectType({
         books : { 
             type : GraphQLList(BookType),
             resolve(parent,args){
-                return _.filter(books,{authorID : parent.id})
+              //  return _.filter(books,{authorID : parent.id})
+              return DB().then(DB => DB.collection("books").find({authorID : parent.id})).then(async (data) => {return await  data.toArray()})
             }
         }
     })
@@ -62,26 +43,32 @@ const RootQuery = new GraphQLObjectType({
             args :{id : {type : GraphQLID}},
             resolve(parent, args){
                 // code to get data from db
-               return _.find(books,{id : args.id})
+               //return _.find(books,{id : args.id})
+               return DB().then(DB => DB.collection("books").findOne({id : args.id}))
+            //    .then(async (data) => {return await  data.toArray()})
             }
         },
         author :{
             type : AuthorType,
             args : {id : {type : GraphQLID}},
             resolve(parent, args){
-                return _.find(authors, {id : args.id})
+               // return _.find(authors, {id : args.id})
+               return DB().then(DB => DB.collection("authors").findOne({id : args.id}))
+            //    .then(async (data) => {return await  data.toArray()})
             }
         },
         books : {
             type : new GraphQLList(BookType),
             resolve(parent,args){
-                return books
+              //  return books
+              return DB().then(DB => DB.collection("books").find({})).then(async (data) => {return await data.toArray()})
             }
         },
         authors : {
             type : new GraphQLList(AuthorType),
             resolve(parent,args){
-                return authors
+               // return authors
+               return DB().then(DB => DB.collection("authors").find({})).then(async (data) => {return await data.toArray()})
             }
         }
     }
